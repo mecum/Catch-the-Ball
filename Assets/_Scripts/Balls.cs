13bdd104f251474741af43c786d7214eb0f5a26d
mecum <145342109+mecum@users.noreply.github.com>
2024-07-27 20:45:37 +0300

    what the hell happend

--- Assets/_Scripts/Balls.cs
+++ Assets/_Scripts/Balls.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Balls : MonoBehaviour
 {        
+{    
+    // Start is called before the first frame update
+    void Start()
+    {       
+        
+    }
+
     // Update is called once per frame
     void Update()
     { 
